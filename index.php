90e43387af6824ff94fbf647e975232351baf039
DanielAnd08 <k5cfy97tyx@privaterelay.appleid.com>
2025-11-20 18:21:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
 </head>
 <body>
  <h1>Register here!</h1>
- <p>Fill in your name and email address, then click <strong>Submit</strong> to
+ <p>Fill in your name and esfsdfsfsdfsdfssdfdsfdsfssfdsfsmail address, then click <strong>Submit</strong> to
 register.</p>
  <form method="post" action="index.php" enctype="multipart/form-data">
  Name
@@ -116,3 +116,4 @@ echo "Insert Failed";
 </body>
 
 </html>
+
